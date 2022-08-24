931c8361b0b0fea00f1e88be78db603411ea921a
jvgmercado <jvgmercado.student@ua.edu.ph>
2022-08-24 21:53:14 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p> I’m Joseph Valentine Mercado but people here call me JV. I grew up in Mexico. I like watching movies, at least once a week. I play basketball on weekends and computer games whenever I get time..</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
